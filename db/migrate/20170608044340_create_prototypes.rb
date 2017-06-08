b197eb27bd5bccd88aa037d0ca425abbd2bae190
r-ume <baymax.blackandwhite@gmail.com>
2017-06-08 16:58:02 +0900

    created tag, comments, likes, prototypes tables (users tables are to be made later)

---
+++ db/migrate/20170608044340_create_prototypes.rb
@@ -0,0 +1,14 @@
+class CreatePrototypes < ActiveRecord::Migration[5.1]
+	def change
+		create_table :prototypes do |t|
+			t.string     :name
+			t.integer    :user_id
+			t.text       :catchcopy
+			t.text       :concept
+			t.integer    :like_count, :default => 0
+			t.string     :image
+
+			t.timestamps 
+		end
+	end
+end
