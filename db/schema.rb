5076b4241f69342ef23aae26f409ad7666f17224
r-ume <baymax.blackandwhite@gmail.com>
2017-06-08 17:10:11 +0900

    replaced comment_id with prototype_id in the like table

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170608073858) do
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
-    t.integer "comment_id"
+    t.integer "prototype_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
