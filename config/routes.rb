b864e2d91faec17c23feafa41445f36d451f27c1
r-ume <baymax.blackandwhite@gmail.com>
2017-06-24 14:56:46 +0900

    because of design modification, replaced resource routing with index, show, and new action only

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :prototypes
+	root 'prototypes#index'
+
+	get  '/prototypes/index'  =>    'prototypes#index'
+	get  '/prototypes/new'    =>    'prototypes#new'
+	get  '/prototypes/show'   =>    'prototypes#show'
 end
