4a99e74169fbab60ca9b1f7f23df8781f004edf7
r-ume <baymax.blackandwhite@gmail.com>
2017-07-03 01:16:58 +0900

    pagination of prototypes in my page is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-
-	@@prototypes_per_page = 9
+	before_action :set_user, only: [:show]
 
 	def show
-		@num_of_current_user_prototypes = current_user.prototypes.count ? current_user.prototypes.count : 0
-		@current_user_prototypes        = current_user.prototypes ? current_user.prototypes: 'hoge'
+		@num_of_current_user_prototypes = @user.prototypes.count ? @user.prototypes.count : 0
+		@current_user_prototypes        = @user.prototypes.paginate(:page => params[:page])
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,8 @@
 class Prototype < ApplicationRecord
 	belongs_to :user
 
+ 	self.per_page = 8
+
 	def posted_time
 		created_at.strftime('%Y/%m/%d %H:%M:%S')
 	end
