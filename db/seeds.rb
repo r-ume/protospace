c3185932def0158dde2286a2e56ddc2fdaa3e55c
r-ume <baymax.blackandwhite@gmail.com>
2017-07-02 23:17:29 +0900

    increased fake prototypes more

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 # coding: utf-8
 
 Prototype.delete_all
-Prototype.create(:name => 'hoge', :user_id => 1, :catchcopy => 'hoge', :concept => 'hoge', :likes_count => 1, :image => 'hoge.jpg')
-Prototype.create(:name => 'fuga', :user_id => 2, :catchcopy => 'fuga', :concept => 'fuga', :likes_count => 2, :image => 'fuga.jpg')
-Prototype.create(:name => 'piyo', :user_id => 3, :catchcopy => 'piyo', :concept => 'piyo', :likes_count => 3, :image => 'piyo.jpg')
+Prototype.create(:name => 'hoge', :user_id => 5, :catchcopy => 'hoge1', :concept => 'hoge1', :likes_count => 1, :image => 'hoge1.jpg')
+Prototype.create(:name => 'fuga', :user_id => 5, :catchcopy => 'fuga1', :concept => 'fuga1', :likes_count => 2, :image => 'fuga1.jpg')
+Prototype.create(:name => 'piyo', :user_id => 5, :catchcopy => 'piyo1', :concept => 'piyo1', :likes_count => 3, :image => 'piyo1.jpg')
+Prototype.create(:name => 'hoge', :user_id => 5, :catchcopy => 'hoge2', :concept => 'hoge2', :likes_count => 1, :image => 'hoge2.jpg')
+Prototype.create(:name => 'fuga', :user_id => 5, :catchcopy => 'fuga2', :concept => 'fuga2', :likes_count => 2, :image => 'fuga2.jpg')
+Prototype.create(:name => 'piyo', :user_id => 5, :catchcopy => 'piyo2', :concept => 'piyo2', :likes_count => 3, :image => 'piyo2.jpg')
+Prototype.create(:name => 'hoge', :user_id => 5, :catchcopy => 'hoge3', :concept => 'hoge3', :likes_count => 1, :image => 'hoge3.jpg')
+Prototype.create(:name => 'fuga', :user_id => 5, :catchcopy => 'fuga3', :concept => 'fuga3', :likes_count => 2, :image => 'fuga3.jpg')
+Prototype.create(:name => 'piyo', :user_id => 5, :catchcopy => 'piyo3', :concept => 'piyo3', :likes_count => 3, :image => 'piyo3.jpg')
 
 Comment.delete_all
 Comment.create(:text => 'hoge', :user_id => 1, :prototype_id => 1)
