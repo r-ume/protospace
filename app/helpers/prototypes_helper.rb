12db4fd8ecf095201a672af09fdf8965ca07c55b
r-ume <baymax.blackandwhite@gmail.com>
2017-07-03 18:18:44 +0900

    changed displayImageOfPrototype into snake_case

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -1,5 +1,5 @@
 module PrototypesHelper
-	def displayImageOfPrototype(prototype)
+	def display_image_of_prototype(prototype)
 		image_path = "#{Rails.root}/public/images/#{prototype.image}"
 		FileTest.exist?(image_path) ? image_tag(prototype.image) : image_tag('noimage-big.png')
 	end	
