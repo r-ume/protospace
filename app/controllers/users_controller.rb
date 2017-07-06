8a9b6bed0a35d746e505e7adb37cc0af90896e0b
r-ume <baymax.blackandwhite@gmail.com>
2017-07-07 02:35:07 +0900

    user's detail is displayed on edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show]
+	before_action :set_user, only: [:show, :edit]
 
 	def show
 		@num_of_current_user_prototypes = @user.prototypes.count || 0
 		@current_user_prototypes        = @user.prototypes.paginate(page: params[:page])
+	end
+
+	def edit
+		
 	end
 
 	private
