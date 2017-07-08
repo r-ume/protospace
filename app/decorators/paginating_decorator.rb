d0589c800c751bcd0c39739b074531cd83eccfd7
r-ume <baymax.blackandwhite@gmail.com>
2017-07-08 15:56:40 +0900

    organizing comments on all decorators

--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,5 +1,6 @@
 class PaginatingDecorator < Draper::CollectionDecorator
 
+	# decorator for paginateion
 	delegate :current_page, :per_page, :offset, :total_entries, :total_pages
-	
+
 end
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,6 @@
 class UserDecorator < ApplicationDecorator
   delegate_all
+
+  decorates :user
   decorates_association :prototype
 end
