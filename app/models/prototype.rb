32dd0a227f432bb71a9446284e49837f16374efb
r-ume <baymax.blackandwhite@gmail.com>
2017-07-11 13:47:57 +0900

    forgot to add status column to prototype image

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ApplicationRecord
 	belongs_to :user
+	has_many :prototype_images
 
  	self.per_page = 8
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711043501) do
+ActiveRecord::Schema.define(version: 20170711044641) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "text"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170711043501) do
     t.bigint "prototype_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["prototype_id"], name: "index_prototype_images_on_prototype_id"
   end
 
