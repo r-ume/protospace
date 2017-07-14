08d39bf9d4cb5fa8fbb68e7348ead51570de0c2f
r-ume <baymax.blackandwhite@gmail.com>
2017-07-15 01:07:43 +0900

    sub images can be previewed as well

--- app/assets/javascripts/prototype_image_preview.js
+++ app/assets/javascripts/prototype_image_preview.js
@@ -3,8 +3,17 @@
 
 $(document).on("ready page:load", function(){
 
-	const mainImageId = "#main-prototype-image";
-	const reader = new FileReader();
+	const mainImageId     = "#main-prototype-image";
+	const mainImageWidth  = 730;
+	const mainImageHeight = 500;
+
+	const subImageId1     = "#sub-prototype-image1";
+	const subImageId2     = "#sub-prototype-image2";
+	const subImageId3     = "#sub-prototype-image3";
+	const subImageWidth   = 213;
+	const subImageHeight  = 200; 
+
+	const reader          =  new FileReader();
 
 	function previewImage(idName){
 		$(idName + ' input[type=file]').change(function(){
@@ -12,10 +21,16 @@ $(document).on("ready page:load", function(){
 			const file = $(this).prop('files')[0];
 
 			if(file.type.match('image.*')) {
+
 				reader.onload = function() {
+					let idNameImage = $(idName + ' img');
+
 					// set src tag into id Name
-					$(idName + ' img').attr('src', reader.result).width(730).height(500);
-					$(idName + ' img').addClass("image-preview");
+					idNameImage.attr('src', reader.result);
+					idNameImage.addClass("image-preview");
+
+					if(idName.match('#main-')) idNameImage.width(mainImageWidth).height(mainImageHeight); 
+					if(idName.match('#sub-')) idNameImage.width(subImageWidth).height(subImageHeight);
 			}
 	
 			reader.readAsDataURL(file);
@@ -24,4 +39,7 @@ $(document).on("ready page:load", function(){
 	}
 
 	previewImage(mainImageId);
+	previewImage(subImageId1);
+	previewImage(subImageId2);
+	previewImage(subImageId3);
 });
