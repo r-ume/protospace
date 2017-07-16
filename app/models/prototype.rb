0c673634718d7b114c31a77624e229d6fb44cb12
r-ume <baymax.blackandwhite@gmail.com>
2017-07-16 13:05:53 +0900

    added accepts_nested_attributes_for method in prototype for prototypes_images in association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,18 @@
 class Prototype < ApplicationRecord
-	belongs_to :user
-	has_many :prototype_images
+	# validation
+	validates :name, :catchcopy, :concept, presence: true
 
+	# pagination
  	self.per_page = 8
+
+ 	# association
+	belongs_to :user
+	has_many :prototype_images, dependent: :destroy
+
+	accepts_nested_attributes_for :prototype_images, allow_destroy: true, reject_if: :rejected_images
+
+	def rejected_images(attributed)
+		attributed['content'].blank?
+	end
+
 end
