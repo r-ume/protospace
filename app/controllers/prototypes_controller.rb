3dcd65ed3cd51dc9311a79d821dea6f6f4b1377d
r-ume <baymax.blackandwhite@gmail.com>
2017-07-16 12:59:08 +0900

    defined index, new, and create action for this pull request

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,21 +1,11 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-
   def index
     @prototypes = Prototype.includes(:user).all
   end
 
-  # GET /prototypes/1.json
-  def show
-  end
-
-  # GET /prototypes/new
   def new
     @prototype = Prototype.new
-  end
-
-  # GET /prototypes/1/edit
-  def edit
+    @prototype.prototype_images.build
   end
 
   def create
@@ -23,42 +13,17 @@ class PrototypesController < ApplicationController
 
     respond_to do |format|
       if @prototype.save
-        format.html { redirect_to @prototype, notice: 'Prototype was successfully created.' }
-        format.json { render :show, status: :created, location: @prototype }
+        format.html { redirect_to :root, notice: 'Prototype was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @prototype.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @prototype.update(prototype_params)
-        format.html { redirect_to @prototype, notice: 'Prototype was successfully updated.' }
-        format.json { render :show, status: :ok, location: @prototype }
-      else
-        format.html { render :edit }
-        format.json { render json: @prototype.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @prototype.destroy
-    respond_to do |format|
-      format.html { redirect_to prototypes_url, notice: 'Prototype was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
+  def prototype_params
+    params.require(:prototype)
+      .permit(:name, :catchcopy, :concept, prototype_images_attributes: [:id, :content, :status])
+      .merge(user_id: current_user.id)
   end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  #def prototype_params
-  #  params.fetch(:prototype, {})
-  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	root 'prototypes#index'
 
 	devise_for :users
-	resources :prototypes, except: :index
+	resources :prototypes, only: [:index, :new, :create]	
 	resources :users, only: [:show, :edit, :update]
 end
