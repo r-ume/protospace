cb90e924e5b4f7d61056ffe48b64366b26d4ce2f
r-ume <baymax.blackandwhite@gmail.com>
2017-07-20 18:55:14 +0900

    succeeded in displaying a main image for prototype in my page

--- app/decorators/prototype_decorator.rb
+++ app/decorators/prototype_decorator.rb
@@ -2,15 +2,20 @@ class PrototypeDecorator < ApplicationDecorator
 	delegate_all
 
 	decorates :prototype
-	decorates_association :user 
+	decorates_association :user
+	decorates_association :prototype_images
 
- 	def posted_time
+	def posted_time
 		object.created_at.strftime('%Y/%m/%d %H:%M:%S')
 	end
 
-	def display_image_of_prototype
-		image_path = "#{Rails.root}/public/images/#{object.image}"
-		FileTest.exist?(image_path) ? h.image_tag(object.image) : h.image_tag('noimage-big.png')
+	def display_image
+		for image in object.prototype_images
+			if image[:status] == 'main'
+				image_path = "#{Rails.root}/public/uploads/#{image.content}"
+			end
+		end
+
+		FileTest.exist?(image_path) ? h.image_tag(image_path) : h.image_tag('noimage-big.png')
 	end	
- 
 end
