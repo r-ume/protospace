bfd5234fdaec54e1368974d1a8e370702c6dd3c1
r-ume <baymax.blackandwhite@gmail.com>
2017-07-22 02:49:43 +0900

    made decorator methods for main and sub images respectively

--- app/decorators/prototype_decorator.rb
+++ app/decorators/prototype_decorator.rb
@@ -1,21 +1,35 @@
 class PrototypeDecorator < ApplicationDecorator
-	delegate_all
+  delegate_all
 
-	decorates :prototype
-	decorates_association :user
-	decorates_association :prototype_images
+  decorates :prototype
+  decorates_association :user
+  decorates_association :prototype_images
 
-	def posted_time
-		object.created_at.strftime('%Y/%m/%d %H:%M:%S')
-	end
+  def posted_time
+    object.created_at.strftime('%Y/%m/%d %H:%M:%S')
+  end
 
-	def image
-		for image in object.prototype_images
-			if image[:status] == 'main' || 0
-				image_path = "#{Rails.root}/public/uploads/#{image.content}"
-			end
-		end
+  def main_image
+    main_images = object.prototype_images.select { |image| image[:status] == 'main' }
+    main_image_path = main_images.first.content.to_s
 
-		FileTest.exist?(image_path) ? h.image_tag(image_path) : h.image_tag('noimage-big.png')
-	end	
+    FileTest.exist?(main_image_path) ? h.image_tag(main_image_path) : h.image_tag('noimage-big.png')
+  end
+
+  def sub_images
+    sub_images = object.prototype_images.select { |image| image[:status] == 'sub' }
+    sub_images.map{ |image| image.content.to_s}
+  end
+
+  def likes
+    object.likes_count
+  end	
+
+  def user_name
+    object.user.name
+  end 
+
+  def user_profile
+    object.user.profile
+  end
 end
