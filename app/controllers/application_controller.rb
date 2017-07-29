8c3e500b847cd177ee1266af06fecfa2843f00fe
r-ume <baymax.blackandwhite@gmail.com>
2017-07-29 21:06:41 +0900

    added protected into configure_permiited_parameters for solid security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permiited_parameters, if: :devise_controller? 
 
+  protected
   def configure_permiited_parameters
   	devise_parameter_sanitizer.permit :sign_up, keys: [:name, :profile, :position, :occupation] 
   end
