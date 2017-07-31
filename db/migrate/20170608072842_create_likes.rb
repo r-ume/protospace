966343b5dd0dfc2ea161b0c64fd380942da48f64
r-ume <baymax.blackandwhite@gmail.com>
2017-07-31 16:17:11 +0900

    Created like table

--- db/migrate/20170608072842_create_likes.rb
+++ db/migrate/20170608072842_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.1]
 	def change
 		create_table :likes do |t|
-			t.integer    :user_id
-			t.integer    :prototype_id
+			t.references :user, index: true
+			t.references :prototype, index: true
 
 			t.timestamps
 		end
