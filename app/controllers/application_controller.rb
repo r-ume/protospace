15c09ba1c8429a6b5081abed18451464e552b988
r-ume <baymax.blackandwhite@gmail.com>
2017-08-02 10:36:44 +0900

    let avatar become a strong parameter in appliction_controller for user to sign up with photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permiited_parameters
-  	devise_parameter_sanitizer.permit :sign_up, keys: [:name, :profile, :position, :occupation] 
+  	devise_parameter_sanitizer.permit :sign_up, keys: [:name, :profile, :position, :occupation, :avatar]
   end
 end
