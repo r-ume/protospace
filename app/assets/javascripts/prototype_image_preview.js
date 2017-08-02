f46b8992ec22977b33716f6fe6f979a1f2a265e6
r-ume <baymax.blackandwhite@gmail.com>
2017-08-02 15:20:30 +0900

    added avatar image class and id in image_preview.js

--- app/assets/javascripts/prototype_image_preview.js
+++ app/assets/javascripts/prototype_image_preview.js
@@ -3,48 +3,67 @@
 
 $(document).on("turbolinks:load", function(){
 
-	const mainImageId      = "#main-prototype-image";
-	const mainImageWidth   = $(mainImageId).width();
-	const mainImageHeight  = $(mainImageId).height();
+    const mainImageId      = "#main-prototype-image";
+    const mainImageWidth   = $(mainImageId).width();
+    const mainImageHeight  = $(mainImageId).height();
 
-	// Collection of custom data(data-sub-image-id)
-	const subImageIdNums = $('div[data-sub-image-id]').map(function () {
-		return $(this).attr('data-sub-image-id');
-	});
-	const firstSubImageId  = "#sub-prototype-image" + subImageIdNums[0];
-	const secondSubImageId = "#sub-prototype-image" + subImageIdNums[1];
-	const subImageWidth    = $('.list-group-item').width();
-	const subImageHeight   = $('.list-group-item').height();
+    // Collection of custom data(data-sub-image-id)
+    const subImageIdNums = $('div[data-sub-image-id]').map(function () {
+        return $(this).attr('data-sub-image-id');
+    });
+    const firstSubImageId  = "#sub-prototype-image" + subImageIdNums[0];
+    const secondSubImageId = "#sub-prototype-image" + subImageIdNums[1];
+    const subImageWidth    = $('.list-group-item').width();
+    const subImageHeight   = $('.list-group-item').height();
 
-	const reader          =  new FileReader();
+    const avatarImageId    = "#avatar-image";
+    const avatarImageClass = ".avatar-image";
+    const avatarImageWidth = $(avatarImageClass).width();
+    const avatarImageHeight = $(avatarImageClass).height();
 
-	function previewImage(idName){
-		$(idName + ' input[type=file]').change(function(){
-			// get an image file
-			const file = $(this).prop('files')[0];
+    const reader          =  new FileReader();
 
-			if(file.type.match('image*')) {
-				reader.onload = function() {
-					let idNameImage = $(idName + ' img');
+    function previewImage(idName){
+        $(idName + ' input[type=file]').change(function(){
 
-					// set src tag into id Name
-					idNameImage.attr('src', reader.result);
-					idNameImage.addClass("image-preview");
+            console.log(idName);
+            // get an image file
+            const file = $(this).prop('files')[0];
 
-					if(idName.match('#main-')) idNameImage.width(mainImageWidth).height(mainImageHeight); 
-					if(idName.match('#sub-')) idNameImage.width(subImageWidth).height(subImageHeight);
-			}
-	
-			reader.readAsDataURL(file);
-			}
-		});
-	}
+            if(file.type.match('image*')) {
+                reader.onload = function() {
+                    const idNameImage = $(idName + ' img');
 
-	previewImage(mainImageId);
-	previewImage(firstSubImageId);
-	previewImage(secondSubImageId);
+                    // set src tag into id Name
+                    idNameImage.attr('src', reader.result);
+                    idNameImage.addClass("image-preview");
 
-	$('.main-prototype-image').width(mainImageWidth).height(mainImageHeight);
-	$('.sub-prototype-image').width(subImageWidth).height(subImageHeight);
+                    if(idName.match('#main-')) idNameImage.width(mainImageWidth).height(mainImageHeight);
+                    if(idName.match('#sub-')) idNameImage.width(subImageWidth).height(subImageHeight);
+                    if(idName.match('#avatar-')) {
+                        idNameImage
+                            .addClass("user-image")
+                            .width(avatarImageWidth)
+                            .height(avatarImageHeight)
+                            .css({
+                                right: '18px'
+                            });
+                    }
+                };
+
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+
+    previewImage(mainImageId);
+    previewImage(firstSubImageId);
+    previewImage(secondSubImageId);
+    previewImage(avatarImageId);
+
+    $('.main-prototype-image').width(mainImageWidth).height(mainImageHeight);
+    $('.sub-prototype-image').width(subImageWidth).height(subImageHeight);
+    $('.avatar-image').width(avatarImageWidth).height(avatarImageHeight);
+
 
 });
