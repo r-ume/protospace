09b2ebdfc568f7a1e0caf914cb689aa1615edcaa
r-ume <baymax.blackandwhite@gmail.com>
2017-08-03 16:11:47 +0900

    let annotate gem list properties in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,16 @@
-class Comment < ApplicationRecord
-  # columns
-  # text, user_id, prototype_id
+# == Schema Information
+#
+# Table name: comments
+#
+#  id           :integer          not null, primary key
+#  text         :text(65535)
+#  user_id      :integer
+#  prototype_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 
+class Comment < ApplicationRecord
   # association
   belongs_to :user
   belongs_to :prototype
