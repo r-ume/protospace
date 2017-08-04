cfacbe18fba1cfb89e038fbe08f96a511920fa89
r-ume <baymax.blackandwhite@gmail.com>
2017-08-04 11:49:04 +0900

    defined set_default_placeholder method and set in before_action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit, :update]
-  before_action :set_prototype,      only:   [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!,          except: [:index, :show, :edit, :update]
+  before_action :set_prototype,               only:   [:show, :edit, :update, :destroy]
+  before_action :set_tag_default_placeholder, only:   [:new, :create, :edit, :update]
 
   def index
     @prototypes = Prototype.eager_load(:user, :prototype_images).all.paginate(page: params[:page]).decorate
@@ -53,10 +54,14 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype)
-      .permit(:name, :catchcopy, :concept, prototype_images_attributes: [:id, :content, :status])
+      .permit(:name, :catchcopy, :concept, :tag_list, prototype_images_attributes: [:id, :content, :status])
   end
 
   def set_prototype
     @prototype = Prototype.find(params[:id]).decorate
+  end
+
+  def set_tag_default_placeholder
+    @tag_default_placeholder = ["Web Design", "UI", "Application"]
   end
 end
