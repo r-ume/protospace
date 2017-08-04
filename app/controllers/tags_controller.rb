9be2f36af259b641df715b377545646d4c19039b
r-ume <baymax.blackandwhite@gmail.com>
2017-08-04 22:43:12 +0900

    defined tags#show action and added view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,9 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
   end
+
+  def show
+    @tag = ActsAsTaggableOn::Tag.find_by(name: params[:tag_name])
+    @prototypes = Prototype.tagged_with(@tag).order(likes_count: :desc).paginate(page: params[:page]).decorate
+  end
 end
