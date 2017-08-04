b1a9ee648ce2639a55ed6fca8b9cbeeefbf70845
r-ume <baymax.blackandwhite@gmail.com>
2017-08-04 11:55:42 +0900

    changed the action that authenticate_user method works in prototype controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,          except: [:index, :show, :edit, :update]
+  before_action :authenticate_user!,          except: [:index, :show]
   before_action :set_prototype,               only:   [:show, :edit, :update, :destroy]
   before_action :set_tag_default_placeholder, only:   [:new, :create, :edit, :update]
 
