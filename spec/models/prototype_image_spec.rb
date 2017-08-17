b8fb04f3a769cf669e87c648580056d86768a9de
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 13:56:44 +0900

    added tests of association with prototype

--- spec/models/prototype_image_spec.rb
+++ spec/models/prototype_image_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
+include ActionDispatch::TestProcess
 
 describe PrototypeImage do
-  describe 'validation' do
-    let(:prototype_image)      { build(:prototype_image) }
-    let(:prototype_main_image) { build(:prototype_image, :main) }
-    let(:prototype_sub_image)  { build(:prototype_image, :sub) }
+  let(:prototype_image)      { build(:prototype_image) }
+  let(:prototype_main_image) { build(:prototype_image, :main) }
+  let(:prototype_sub_image)  { build(:prototype_image, :sub) }
 
+  describe 'validation' do
     context 'with all necessary attributes' do
       it 'main image is valid' do
         expect(prototype_main_image).to be_valid
@@ -34,6 +35,28 @@ describe PrototypeImage do
       it 'is invalid without status attribute' do
         prototype_image.valid?
         expect(prototype_image.errors[:status]).to include('can\'t be blank')
+      end
+    end
+
+    it 'wrong format of image file' do
+      expect{
+        prototype_main_image.content = fixture_file_upload('spec/fixtures/baymax.tiff', 'image/tiff')
+      }.to raise_error(CarrierWave::IntegrityError)
+    end
+  end
+
+  describe 'association' do
+    context 'is associated with prototype' do
+      it 'if status is main' do
+        prototype  = create(:prototype)
+        prototype_main_image = create(:prototype_image, :main, prototype: prototype)
+        expect(prototype_main_image.prototype).to eq prototype
+      end
+
+      it 'if status is sub' do
+        prototype  = create(:prototype)
+        prototype_sub_image = create(:prototype_image, :sub, prototype: prototype)
+        expect(prototype_sub_image.prototype).to eq prototype
       end
     end
   end
