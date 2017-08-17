b58e6ca1ed7b68e3d103e07235748cc84912c52d
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 14:19:09 +0900

    added tests of associations with prototype and user in like_spec

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  prototype_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+require 'rails_helper'
+
+describe Like do
+  context 'validation' do
+    it 'is associated with prototype' do
+      prototype = create(:prototype)
+      like = create(:like, prototype: prototype)
+      expect(like.prototype).to eq prototype
+    end
+
+    it 'is associated with user' do
+      user = create(:user)
+      like = create(:like, user: user)
+      expect(like.user).to eq user
+    end
+  end
+end
