a2a6d4a6a1fadcddae6637ec8beec9c9e8185a99
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 11:18:43 +0900

    added tests of association with prototype_images

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -17,13 +17,23 @@ include ActionDispatch::TestProcess
 
 describe Prototype do
   describe 'associations' do
-    let(:user) { create(:user) }
-    let(:prototype)  { create(:prototype) }
-
     context 'with a user' do
+      let(:user) { create(:user) }
+      let(:prototype)  { create(:prototype) }
       it 'is associated with an user' do
         prototype.user = user
         expect(prototype.user).to eq user
+      end
+    end
+
+    context 'with prototype images' do
+      let(:additional_sub_prototype_images) { 3 }
+      let!(:prototype) { create(:prototype, :with_sub_images, default_sub_images_num: additional_sub_prototype_images) }
+
+      it 'deletes the prototype sub images when a prototype gets deleted' do
+        # total_sub_images_num = addtional_sub_prototype_images + default_sub_images_num = 3 + 3
+        total_sub_images_num = additional_sub_prototype_images * 2
+        expect{ prototype.destroy }.to change{ PrototypeImage.count }.by(-1 * total_sub_images_num)
       end
     end
 
@@ -31,7 +41,9 @@ describe Prototype do
       let(:additional_comments_num) { 5 }
       let!(:prototype) { create(:prototype, :with_comments, default_comments_num: additional_comments_num) }
       it 'deletes the comments when a prototype gets deleted' do
-        expect{ prototype.destroy }.to change{ Comment.count }.by(-1 * (additional_comments_num * 2))
+        # total_comments_num = addtional_comments_num + default_comments_num = 5 + 5
+        total_comments_num = additional_comments_num * 2
+        expect{ prototype.destroy }.to change{ Comment.count }.by(-1 * total_comments_num)
       end
     end
   end
