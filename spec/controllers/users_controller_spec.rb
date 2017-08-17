c65ec09a141d498b47d048820a8167b85c2e6e02
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 15:56:36 +0900

    added tests of user#edit action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,17 +7,31 @@ describe UsersController do
   describe 'with user login' do
     before { login_user }
 
-    before :each do
-      get :show, params: { id: user.id }
-    end
-
     context 'GET #show' do
+      before :each do
+        get :show, params: { id: user.id }
+      end
+
       it 'assigns the requested to @user' do
         expect(assigns(:user)).to eq user
       end
 
       it 'renders the :show templates' do
         expect(response).to render_template :show
+      end
+    end
+
+    context 'GET #edit' do
+      before :each do
+        get :edit, params: { id: user.id }
+      end
+
+      it 'assigns the requested user to @user' do
+        expect(assigns(:user)).to eq user
+      end
+
+      it 'renders the :edit template' do
+        expect(response).to render_template :edit
       end
     end
   end
