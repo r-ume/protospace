cbe5612d5a2a1348b0c7a62569bca0ba76949537
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 16:54:28 +0900

    added authenticate_user method in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,30 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user,           only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
 
-	def show
-		@num_of_current_user_prototypes = @user.prototypes.size || 0
-		@current_user_prototypes        = @user.prototypes.paginate(page: params[:page])
-	end
+  def show
+    @num_of_current_user_prototypes = @user.prototypes.size || 0
+    @current_user_prototypes        = @user.prototypes.paginate(page: params[:page])
+  end
 
-	def edit
+  def edit
 
-	end
+  end
 
-	def update
-		if @user.update(user_params)
-			redirect_to root_path, notice: "edited your account in successfully."
-		else
-			render :edit
-		end
-	end
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: "edited your account in successfully."
+    else
+      render :edit
+    end
+  end
 
-	private
-	def set_user
-		@user = User.find(params[:id])
-	end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-	def user_params
-		params.require(:user).permit(:name, :email, :position, :profile, :occupation, :avatar)
-	end
+  def user_params
+    params.require(:user).permit(:name, :email, :position, :profile, :occupation, :avatar)
+  end
 end
