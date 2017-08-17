c1e9caa324b396273dfa7233c7bf5ecf715d2dd0
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 15:51:12 +0900

    include devise and controller_macro in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,9 +31,6 @@ require 'support/controller_macros'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
-  config.include FactoryGirl::Syntax::Methods
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -61,4 +58,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
 end
