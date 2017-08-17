261c16739acf80b6df716d6b4bf97d0b3f3f6601
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 11:18:13 +0900

    made with_sub_images trait

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -21,6 +21,20 @@ FactoryGirl.define do
     created_at  { Faker::Time.between(7.days.ago, Time.now) }
     association :user, factory: :user, name: Faker::Pokemon.name
 
+    trait :with_sub_images do
+      ignore do
+        default_sub_images_num 3
+      end
+
+      after(:build) do |prototype, evaluator|
+        prototype.prototype_images << build_list(:sub_images, evaluator.default_sub_images_num)
+      end
+
+      after(:create) do |prototype, evaluator|
+        prototype.prototype_images << create_list(:sub_images, evaluator.default_sub_images_num)
+      end
+    end
+
     trait :with_comments do
       # 参考url
       # https://stackoverflow.com/questions/38573131/what-is-the-purpose-of-a-transient-do-block-in-factorygirl-factories
@@ -37,5 +51,6 @@ FactoryGirl.define do
         prototype.comments << create_list(:comment, evaluator.default_comments_num)
       end
     end
+
   end
 end
