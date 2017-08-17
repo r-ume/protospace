af55ae1a64ca2f726891b5e5993195df0e83b0e5
r-ume <baymax.blackandwhite@gmail.com>
2017-08-17 10:01:09 +0900

    type(wrote prototype in likes file)

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -10,7 +10,7 @@
 #
 
 FactoryGirl.define do
-  factory :prototype_image do
+  factory :like do
     association :user,      factory: :user,      name: Faker::Pokemon.name
     association :prototype, factory: :prototype, name: Faker::HarryPotter.book
   end
